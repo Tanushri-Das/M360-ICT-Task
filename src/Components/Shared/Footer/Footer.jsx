34293272023cbb050fec61a2ed4468ff4bb6d9d5
tanu <dastanushri402@gmail.com>
2024-03-16 20:56:42 +0600

    add responsive footer

--- src/Components/Shared/Footer/Footer.jsx
+++ src/Components/Shared/Footer/Footer.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { Layout, Typography } from "antd";
+
+const { Footer: AntFooter } = Layout;
 
 const Footer = () => {
+  const { Title } = Typography;
   return (
-    <div>Footer</div>
-  )
-}
+    <>
+      <AntFooter style={{ textAlign: "center" }}>
+        <Title level={4}>© 2024 - All rights reserved by Tanushri Das</Title>
+      </AntFooter>
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
